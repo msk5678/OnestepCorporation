9d19969856365778d7153257bd0cc6953a34d11f
monocsp <monocsp@naver.com>
2021-05-21 17:00:54 +0900

    deleting

--- onestep_rezero/lib/login/custom_exeption.dart
+++
@@ -1,8 +0,0 @@
-class CustomException implements Exception {
-  final String message;
-// final String? message;
-  const CustomException({this.message = 'Something went wrong!'});
-
-  @override
-  String toString() => 'CustomException { message : $message}';
-}
