31aa14740a54f1f810107e711ad3e9597d7d946f
ungchun <50910456+ungchun@users.noreply.github.com>
2021-07-01 23:06:20 +0900

    log edit

--- onestep_rezero/lib/login/dml/authDml.dart
+++ onestep_rezero/lib/login/dml/authDml.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:onestep_rezero/login/model/user.dart' as MYUSER;
-import '../../loggedInWidget.dart';
+// import 'package:onestep_rezero/login/model/user.dart' as MYUSER;
+// import '../../loggedInWidget.dart';
 
 Future<void> authEmailDML(
     List<UserInfo> user, String uniName, String email) async {
@@ -12,12 +12,12 @@ Future<void> authEmailDML(
     "authTime": DateTime.now().millisecondsSinceEpoch
   });
 
-  var ref = FirebaseFirestore.instance.collection('user');
+  // var ref = FirebaseFirestore.instance.collection('user');
 
-  var time = DateTime.now().microsecondsSinceEpoch;
-  DocumentSnapshot userRecord = await ref.doc(user.single.uid).get();
-  currentUserModel = MYUSER.User.fromDocument(userRecord);
-  ref.doc(currentUserModel.uid).collection("log").doc(time.toString()).set({
-    "loginTime": time,
-  });
+  // var time = DateTime.now().microsecondsSinceEpoch;
+  // DocumentSnapshot userRecord = await ref.doc(user.single.uid).get();
+  // currentUserModel = MYUSER.User.fromDocument(userRecord);
+  // ref.doc(currentUserModel.uid).collection("log").doc(time.toString()).set({
+  //   "loginTime": time,
+  // });
 }
